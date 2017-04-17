7059f4a41d8f5c667038b83d8201db28e669fdc7
William Dockery <zxmnv@icloud.com>
2017-04-17 12:17:49 -0500

    this is my initial commit

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,12 +2,32 @@ import Vapor
 
 let drop = Droplet()
 
-drop.get { req in
+drop.get("hello") { request in
+	return "Hello, world!"
+}
+
+drop.get("/") { request in
+    return "Hello, World!"
+}
+
+drop.get("/name",":name") {request in
+    if let name = request.parameters["name"]?.string {
+        return "Hello, \(name)!"
+    }
+    return "Error retrieving parameters."
+}
+
+drop.get("/view") { request in
+    return try drop.view.make("view.html")
+}
+
+/*
     return try drop.view.make("welcome", [
     	"message": drop.localization[req.lang, "welcome", "title"]
     ])
 }
 
 drop.resource("posts", PostController())
+*/
 
 drop.run()
